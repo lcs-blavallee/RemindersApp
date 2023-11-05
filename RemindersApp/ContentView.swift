4c70137e833e341101c15d43756fd7e240ecf88a
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-05 18:49:49 -0500

    Further progress today

--- RemindersApp/ContentView.swift
+++ RemindersApp/ContentView.swift
@@ -13,20 +13,53 @@ struct ContentView: View {
             Color.white
                 .ignoresSafeArea()
             NavigationStack {
-                List {
-                    Group{
-                        
-                        ReminderView(reminder:"Call auto body shop", reminderColor: .black, reminderDate: "2023-11-16",reminderDateColor: .gray)
+                    List {
+                        Group{
+                            HStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(Color.gray)
+                                ReminderView(reminder:"Call auto body shop", reminderColor: .black, reminderDate: "2023-11-16",reminderDateColor: .gray)
+                            }
                             .listRowSeparator(.hidden, edges: .top)
-                        ReminderView(reminder:"Feed Piper", reminderColor: .black, reminderDate: "Tomorrow, 8:00 AM, Daily",reminderDateColor: .gray)
-                        ReminderView(reminder:"Write lesson about interactive apps", reminderColor: .black, reminderDate: "Tomorrow, 6:00 PM",reminderDateColor: .gray)
-                        ReminderView(reminder:"Take Piper to the vet", reminderColor: .black, reminderDate: "2023-11-29, 3:00 PM",reminderDateColor: .gray)
-                        ReminderView(reminder:"Drop off donations at Goodwill", reminderColor: .black, reminderDate: "2023-11-11",reminderDateColor: .gray)
-                        ReminderView(reminder:"Call plumber about clog in sink", reminderColor: .black, reminderDate: "Tomorrow, 2:00 PM",reminderDateColor: .gray)
-                        ReminderView(reminder:"Call dentist to schedule appointment", reminderColor: .black, reminderDate: "2023-11-06, 4:00 PM",reminderDateColor: .gray)
-                        ReminderView(reminder:"Call mechanic to get TARDIS repaired", reminderColor: .black, reminderDate: "Tomorrow, 12:00 PM",reminderDateColor: .gray)
+                            HStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(Color.gray)
+                                ReminderView(reminder:"Feed Piper", reminderColor: .black, reminderDate: "Tomorrow, 8:00 AM, Daily",reminderDateColor: .gray)
+                            }
+                            HStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(Color.gray)
+                                ReminderView(reminder:"Write lesson about interactive apps", reminderColor: .black, reminderDate: "Tomorrow, 6:00 PM",reminderDateColor: .gray)
+                            }
+                            HStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(Color.gray)
+                                ReminderView(reminder:"Take Piper to the vet", reminderColor: .black, reminderDate: "2023-11-29, 3:00 PM",reminderDateColor: .gray)
+                            }
+                            HStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(Color.gray)
+                                ReminderView(reminder:"Drop off donations at Goodwill", reminderColor: .black, reminderDate: "2023-11-11",reminderDateColor: .gray)
+                            }
+                            HStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(Color.gray)
+                                ReminderView(reminder:"Call plumber about clog in sink", reminderColor: .black, reminderDate: "Tomorrow, 2:00 PM",reminderDateColor: .gray)
+                            }
+                            HStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(Color.gray)
+                                ReminderView(reminder:"Call dentist to schedule appointment", reminderColor: .black, reminderDate: "2023-11-06, 4:00 PM",reminderDateColor: .gray)
+                            }
+                            HStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(Color.gray)
+                                ReminderView(reminder:"Call mechanic to get TARDIS repaired", reminderColor: .black, reminderDate: "Tomorrow, 12:00 PM",reminderDateColor: .gray)
+                            }
+                            
+                        }
                     }
-                }
+                
                 .font(.system(size: 20,design: .rounded))
                 .listStyle(.plain)
                 .navigationTitle("To Do")
